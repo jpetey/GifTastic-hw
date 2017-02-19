3434a1ca95ad252b8de4c14ac57eda1d9cb9cb40
jpetey <jeremee.peters@gmail.com>
2017-02-18 20:16:27 -0800

    HTML, CSS, JS files created and some code work done

---
+++ script.js
@@ -0,0 +1,58 @@
+// Define array of topics
+
+var topics = [  "Double Dare",
+				"Rocko's Modern Life", 
+				// "All That", 
+				// "lengthends of the Hidden Temple",
+				// "Doug",
+				// "Salute Your Shorts",
+				// "Ren & Stimpy",
+				// "Clarissa Explains It All",
+				// "Rugrats",
+				// "Are You Afraid of the Dark?",
+				// "Spongebob Squarepants",
+				// "KaBlam!",
+				// "The Secret World of Alex Mack",
+				// "Aaahh!!! Real Monsters"
+			]; 
+
+// CREATE BUTTONS FOR EACH PRE-DEFINED TOPIC
+// Using a for-loop to iterate through each item in topics array...
+for (var i = 0; i < topics.length; i++) {
+	
+	//... create a <button> container for each array
+	var nickShowBtn = $("<button>");
+	console.log(nickShowBtn);
+	
+	//... add class for CSS styling
+	nickShowBtn.addClass("tvshow-buttons");
+
+	//... assign a data-topic data-attribute to each topic equal to it's name
+	nickShowBtn.attr("data-topic", topics[i]);
+	console.log(nickShowBtn.data("topic"));
+	
+	//... populate each new <div> with text
+	nickShowBtn.text(topics[i]);
+	
+	//... append to <div> with id = 'button-container'
+	$("#button-container").append(nickShowBtn);
+}
+
+// DEFINE WHAT HAPPENS WHEN PREDEFINED BUTTONS ARE CLICKED
+    // Bind click event to elements with class="tvshow-buttons"
+    $(".tvshow-buttons").on("click", function() {
+
+      // Assign the URL that we wish to query, to a variable
+      	// insert API key as needed
+      	// limit results to 10
+      var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        topics.name + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+      // Create your AJAX call: Request dta from API using AJAX
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+
+      // Process AJAX call response
+
